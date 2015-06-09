fb390d07e59085b314f606fd6677a7d651da3d39
Taylor Hamling <0603671@live.com>
2015-06-09 14:57:36 +1200

    restarting

--- www/js/index.js
+++ www/js/index.js
@@ -1,68 +1,91 @@
-var app = (function()
-{
-    // Application object.
-    var app = {};
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+		app.startMonitoring();
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-    // Dictionary of beacons.
-    var beacons = {};
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-    // Timer that displays list of beacons.
-    var updateTimer = null;
+        console.log('Received Event: ' + id);
+    },
 
-    app.initialize = function()
-    {
-        document.addEventListener('deviceready', onDeviceReady, false);
-    };
+	logToDom: function (message) {
+		var e = document.createElement('label');
+		e.innerText = message;
 
-    function onDeviceReady()
-    {
-        // Start tracking beacons!
-        startScan();
+		var br = document.createElement('br');
+		var br2 = document.createElement('br');
+		document.body.appendChild(e);
+		document.body.appendChild(br);
+		document.body.appendChild(br2);
 
-        // Display refresh timer.
-        updateTimer = setInterval(displayBeaconList, 1000);
-    }
+		window.scrollTo(0, window.document.height);
+	},
 
-    function startScan()
-    {
-
-		var logToDom = function (message) {
-			var e = document.createElement('label');
-			e.innerText = message;
-
-			var br = document.createElement('br');
-			var br2 = document.createElement('br');
-			document.body.appendChild(e);
-			document.body.appendChild(br);
-			document.body.appendChild(br2);
-
-			window.scrollTo(0, window.document.height);
-		};
-
+	startMonitoring: function(){	
 		var delegate = new cordova.plugins.locationManager.Delegate();
 
 		delegate.didDetermineStateForRegion = function (pluginResult) {
 
-			logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+		app.logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
 
-			cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
-				+ JSON.stringify(pluginResult));
+		cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+			+ JSON.stringify(pluginResult));
 		};
 
 		delegate.didStartMonitoringForRegion = function (pluginResult) {
 			console.log('didStartMonitoringForRegion:', pluginResult);
 
-			logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+			app.logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
 		};
 
 		delegate.didRangeBeaconsInRegion = function (pluginResult) {
-			logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+			app.logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
 		};
 
-		var uuid = 'b9407f30-f5f8-466e-aff9-25556b57fe6d';
-		var identifier = 'beaconOnTheSpeaker';
-		var minor = 41219;
-		var major = 30846;
+		var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
+		var identifier = 'beaconOnTheMacBooksShelf';
+		var minor = 1000;
+		var major = 5;
 		var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
 
 		cordova.plugins.locationManager.setDelegate(delegate);
@@ -74,9 +97,8 @@ var app = (function()
 		cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
 			.fail(console.error)
 			.done();
-        }
+	}
+	
+};
 
- 
-
-    return app;
-})();
+app.initialize();
