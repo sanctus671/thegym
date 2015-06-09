9a77f06e8c63d1a3e194ed4dbd3e26f12422f284
Taylor Hamling <0603671@live.com>
2015-06-09 15:42:39 +1200

    .

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		app.startMonitoring();
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -46,59 +45,54 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-
-	logToDom: function (message) {
-		var e = document.createElement('label');
-		e.innerText = message;
-
-		var br = document.createElement('br');
-		var br2 = document.createElement('br');
-		document.body.appendChild(e);
-		document.body.appendChild(br);
-		document.body.appendChild(br2);
-
-		window.scrollTo(0, window.document.height);
-	},
-
-	startMonitoring: function(){	
-		var delegate = new cordova.plugins.locationManager.Delegate();
-
-		delegate.didDetermineStateForRegion = function (pluginResult) {
-
-		app.logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
-
-		cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
-			+ JSON.stringify(pluginResult));
-		};
-
-		delegate.didStartMonitoringForRegion = function (pluginResult) {
-			console.log('didStartMonitoringForRegion:', pluginResult);
-
-			app.logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
-		};
-
-		delegate.didRangeBeaconsInRegion = function (pluginResult) {
-			app.logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
-		};
-
-		var uuid = 'DA5336AE-2042-453A-A57F-F80DD34DFCD9';
-		var identifier = 'beaconOnTheMacBooksShelf';
-		var minor = 1000;
-		var major = 5;
-		var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
-
-		cordova.plugins.locationManager.setDelegate(delegate);
-
-		// required in iOS 8+
-		cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
-		// or cordova.plugins.locationManager.requestAlwaysAuthorization()
-
-		cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
-			.fail(console.error)
-			.done();
-	}
-	
+    }
 };
 
-app.initialize();
+var logToDom = function (message) {
+    var e = document.createElement('label');
+    e.innerText = message;
+
+    var br = document.createElement('br');
+    var br2 = document.createElement('br');
+    document.body.appendChild(e);
+    document.body.appendChild(br);
+    document.body.appendChild(br2);
+
+    window.scrollTo(0, window.document.height);
+};
+
+var delegate = new cordova.plugins.locationManager.Delegate();
+
+delegate.didDetermineStateForRegion = function (pluginResult) {
+
+    logToDom('[DOM] didDetermineStateForRegion: ' + JSON.stringify(pluginResult));
+
+    cordova.plugins.locationManager.appendToDeviceLog('[DOM] didDetermineStateForRegion: '
+        + JSON.stringify(pluginResult));
+};
+
+delegate.didStartMonitoringForRegion = function (pluginResult) {
+    console.log('didStartMonitoringForRegion:', pluginResult);
+
+    logToDom('didStartMonitoringForRegion:' + JSON.stringify(pluginResult));
+};
+
+delegate.didRangeBeaconsInRegion = function (pluginResult) {
+    logToDom('[DOM] didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult));
+};
+
+var uuid = 'b9407f30-f5f8-466e-aff9-25556b57fe6d';
+var identifier = 'beaconOnTheSpeaker';
+var minor = 41219;
+var major = 30846;
+var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+cordova.plugins.locationManager.setDelegate(delegate);
+
+// required in iOS 8+
+cordova.plugins.locationManager.requestWhenInUseAuthorization(); 
+// or cordova.plugins.locationManager.requestAlwaysAuthorization()
+
+cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion)
+    .fail(console.error)
+    .done();
